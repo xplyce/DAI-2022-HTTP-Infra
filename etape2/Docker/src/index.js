baf3b802dc22e24653d448ffb94d1748beb9e5d0
Nicolas Philipp <nicolas.philipp@heig-vd.ch>
2022-12-12 11:51:42 +0100

    requête get mise en place dans index.js

--- etape2/Docker/src/index.js
+++ etape2/Docker/src/index.js
@@ -1,4 +1,20 @@
 var Chance = require('chance');
 var chance = new Chance();
 
-console.log(chance.animal());
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send( generateAnimals() );
+});
+
+app.get('/test', function(req, res) {
+    res.send("Hola");
+});
+
+app.listen(3000, function () {
+   console.log('Accepting HTTP request on port 3000')
+});
+
+//dans un fichier appart qu'on import
+function generateAnimals() {}
--- etape2/readme.md
+++ etape2/readme.md
@@ -1,2 +1,3 @@
 liens utiles :
 https://nodejs.org/en/docs/guides/anatomy-of-an-http-transaction/
+https://expressjs.com/en/starter/installing.html
