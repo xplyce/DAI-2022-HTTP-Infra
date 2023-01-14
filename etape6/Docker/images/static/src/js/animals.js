eb6db50b57c77df0c36fec04dd17639f11f83fc2
xplyce <xplicite.18@hotmail.com>
2023-01-14 11:02:12 +0100

    ajout etape 6

---
+++ etape6/Docker/images/static/src/js/animals.js
@@ -0,0 +1,11 @@
+setInterval(async() => {
+
+    const animals = await fetch('/api/').then(response => response.json());
+
+    if (animals.length > 0) {
+        send = " : [typeOfAnimal : " + animals[0].typeOfAnimal +
+            ", ageOfAnimal : " + animals[0].ageOfAnimal + ", countryOfAnimal :"
+            + animals[0].countryOfAnimal + "]";
+    }
+
+    document.getElementById("api-animals").innerHTML = send}, 3000)
