ba6a1fb54e6bb487c7b396bb05e5468b7891b1d4
Nicolas Philipp <nicolas.philipp@heig-vd.ch>
2023-01-14 10:46:19 +0100

    copie etape 4

--- etape5/Docker/images/static/src/js/animals.js
+++ etape5/Docker/images/static/src/js/animals.js
@@ -3,9 +3,9 @@ setInterval(async() => {
     const animals = await fetch('/api/').then(response => response.json());
 
     if (animals.length > 0) {
-        send = " : [typeOfAnimal : " + animals[0].typeOfAnimal +
+        send = "[typeOfAnimal : " + animals[0].typeOfAnimal +
             ", ageOfAnimal : " + animals[0].ageOfAnimal + ", countryOfAnimal :"
             + animals[0].countryOfAnimal + "]";
     }
 
-    document.getElementById("api-animals").innerHTML = send}, 3000)
+    document.getElementById("api-animals").innerHTML = send}, 5000)
